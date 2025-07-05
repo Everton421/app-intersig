01b0051e5db5df265311c70da0323c895896ac69
unknown <evertonjose421@gmail.com>
2025-07-04 22:46:12 -0300

    '04072025'

--- src/utils/id-generator.ts
+++ src/utils/id-generator.ts
@@ -2,11 +2,19 @@ import { usePedidos } from "../database/queryPedido/queryPedido";
 
 
 
-export const generatorId = ()=>{
-      const useQuerypedidos = usePedidos();
-    
-      const ultimoIten = useQuerypedidos.selectLastId();
+export const generatorId = (code: number, codeComplement: number) => {
+      const aux = code.toString().length;
 
-            
+      if (aux < 10) {
+            let i = 10 - aux
+            let comp = '0'
+            while (true) {
+                  if (comp.length == i) {
+                        break
+                  }
+                  comp += '0'
+            }
+            console.log(comp + code + '-' + codeComplement)
+      }
 
 }
